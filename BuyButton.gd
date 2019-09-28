9d0b9541fb336c53c7c6fb2b2677142c082b549a
Lucas Jahn <lucas@krautnerds.de>
2019-09-28 20:28:31 +0200

    added InAppPurchase

---
+++ BuyButton.gd
@@ -0,0 +1,3 @@
+extends TextureButton
+
+export var product_id = ''
--- Store.gd
+++ Store.gd
@@ -1,15 +1,22 @@
 extends MarginContainer
 
+var InAppStore = Engine.get_singleton("InAppStore")
+
 onready var button_list = $StoreWrapper/StoreRect/Container/ButtonsContainer
 
 func _ready():
 	for button in button_list.get_children(): 
-		button.get_child(0).connect('pressed', self, '_on_buy_clicked')
+		var buttonNode = button.get_child(0)
+		buttonNode.connect('pressed', self, '_on_buy_clicked', [buttonNode.product_id])
 	
 	
-func _on_buy_clicked():
-	print('Gekoooft!')
-
+func _on_buy_clicked(product_id):
+    var result = InAppStore.purchase( { "product_id": product_id } )
+	
+    if result == OK:
+        print('result')
+    else:
+        print('error')
 
 
 func _on_CloseButton_pressed():
